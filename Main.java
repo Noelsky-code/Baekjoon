f225c716ef108b3441949cb93218f603c6d151c3
Noelsky-code <hansorly@naver.com>
2021-05-18 18:24:54 +0900

    find_cycle에 무한루프 발생 .

--- Main.java
+++ Main.java
@@ -6,55 +6,58 @@ import java.io.*;
 
 public class Main {
     
-// 9252 , LCS2 
-// 수업시간에 배운 lCS 활용 . 
-
+    static int answer;
+// 9466 , 텀 프로젝트 . 
+// 1, 2는 서로 원하지 않으므로 실패 , 3은 혼자 팀 했기떄문에 팀 성립 , 4 -> 7 -> 6 -> 4 싸이클 .. 싸이클 !!
+// 싸이클이 성립되면 팀 성립 
+// 싸이클에 들어가지 않는 노드 구하면 됨 . 
+// 자기 자신 가르키는건 pass 
+// 싸이클 체크 
+    
     public static void main(String[] args) throws IOException {
         // Scanner scanner = new Scanner(Scolstem.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-        int l1 =s1.length();
-        int l2 =s2.length();
-        int dp[][] = new int[l1+1][l2+1];
-        int arr[][] = new int[l1+1][l2+1];
-        StringBuilder sb = new StringBuilder();
-        int cnt= 0; 
-        for(int i=1;i<=l1;i++){
-            for(int j=1;j<=l2;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                    cnt = Math.max(dp[i][j],cnt);
-                    arr[i][j]=1;
-                    //sb.append(s1.charAt(i-1));
-                    
+        int t = Integer.parseInt(br.readLine());
+        while(t--!=0){
+            
+            int n = Integer.parseInt(br.readLine());
+            answer =n;
+            boolean[] checked = new boolean[n+1];
+            HashSet<Integer> set = new HashSet<Integer>();
+            int arr[] = new int[n+1];
+            String s[] = br.readLine().split(" ");
+            for(int i =1 ; i<=n ; i++ ){
+                arr[i] = Integer.parseInt(s[i-1]);                
+            }
+
+            for(int i=0;i<n;i++){
+                if(!set.contains(i)){
+                    LinkedList<Integer> list = new LinkedList<Integer>();
+                    list.add(i);
+                    find_cycle(i,arr[i],arr,set,list);
                 }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
             }
-        }
-        System.out.println(cnt);
-        int i=l1;int j=l2;
-        while(i!=0&&j!=0){
-            if(arr[i][j]==1){
-                sb.append(s1.charAt(i-1));
-                i--;j--;
-            }
-            else if(dp[i-1][j]<dp[i][j-1]){
-                j--;
-            }
-            else{
-                i--;
-            }
-        }
-        if(cnt==0){
-        }
-        else{
-            System.out.println(sb.reverse().toString());
+            System.out.println(n-set.size());
+            
         }
         
+    }
+    static void find_cycle(int root,int from,int[] arr,HashSet<Integer> set,LinkedList<Integer> list){//dfs 방식 , 싸이클을 확인함  
+        if(set.contains(arr[from])){// 만약 이미 사이클이 형성된 곳을 연결하려고 하면
+            return;     
+        }
+        
+        if(root == arr[from]){
+            int size = list.size();
+            while(size--!=0){
+                set.add(list.poll());
+            }
+            return;
+        }
+
+        list.add(arr[from]);
+        find_cycle(root,arr[from],arr,set,list);
     }
     
 }
